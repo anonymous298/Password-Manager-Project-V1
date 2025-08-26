f056c1598cc2530dfd685b096e3d378a01a41fcf
anonymous298 <tackletalha@gmail.com>
2025-08-26 21:52:43 +0500

    fetching data from localstorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import PasswordManager from './components/PasswordManager'
 import { contextState } from './context/context'
 import React from 'react';
+
 import { ToastContainer } from 'react-toastify';
 
 function App() {
   const [passwordObj, setPasswordObj] = useState([])
   
+  useEffect(() => {
+    const storedPasswords = JSON.parse(localStorage.getItem("passwords")) || [];
+    console.log(storedPasswords)
+
+    storedPasswords ? setPasswordObj(storedPasswords) : setPasswordObj([]);
+  }, [])
+  
+
   return (
     <>
       <div className='h-[100vh] flex flex-col justify-between'>
