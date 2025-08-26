bc75ff943f82b7bb5cf063ae06a5123b61da97a4
anonymous298 <tackletalha@gmail.com>
2025-08-26 21:53:19 +0500

    navbar responsivenss changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div className='bg-[#124170]'>
-      <nav className='flex w-[80%] items-center justify-between mx-auto p-1.5'>
+      <nav className='flex w-[80%] items-center justify-between mx-auto p-1.5 max-[500px]:w-[88%] max-[400px]:w-[96%]'>
         <h1 className="logo text-white font-bold text-[22px] cursor-pointer"><span className='text-[#67C090]'>&lt;</span>Pass<span className='text-[#67C090]'>Fortress</span><span className='text-[#67C090]'>/&gt;</span></h1>
 
         <div className=' border-2 border-[#67C090] px-5 py-1 rounded-[20px] font-bold text-white flex items-center justify-center hover:bg-[#67C090] cursor-pointer transition-all'>Login</div>
