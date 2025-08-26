a7a4c78d8f4164b3598c0b2480de101cf2bd3e80
anonymous298 <tackletalha@gmail.com>
2025-08-26 18:05:32 +0500

    password container rendering component created

---
+++ src/components/PasswordRendering.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from 'react'
+import { useContext } from 'react'
+import { contextState } from '../context/context'
+
+const PasswordRendering = () => {
+    const {passwordObj, setPasswordObj} = useContext(contextState);
+    console.log(passwordObj)
+
+  return (
+    <div className='flex flex-col gap-2'>
+        <h1 className="title text-[20px] font-bold">Your Passwords</h1>
+
+        {passwordObj[0] ? '' : <h3>There is nothing to show</h3>}
+
+        {passwordObj[0] && 
+            <div className='h-[250px] max-h-[300px] overflow-y-auto overflow-x-auto'>
+                <table className='w-[100%]'>
+                    <thead>
+                        <tr className='bg-green-700 text-center'>
+                            <th className='text-white font-semibold p-1.5 rounded-tl-[5px]'>Site</th>
+                            <th className='text-white font-semibold p-1.5'>Username</th>
+                            <th className='text-white font-semibold p-1.5'>Password</th>
+                            <th className='text-white font-semibold p-1.5 rounded-tr-[5px]'>Actions</th>
+                        </tr>
+                    </thead>
+
+                    
+                    <tbody>
+                        {passwordObj.map((val) => {
+                            return (
+                                <tr className='bg-green-200 text-center'>
+                                    <td className='p-2'>{val.websiteURL}</td>
+                                    <td className='p-2'>{val.username}</td>
+                                    <td className='p-2'>{val.password}</td>
+                                    <td className='p-2'>
+                                        <button><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
+                                                <path d="M21.731 2.269a2.625 2.625 0 0 0-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 0 0 0-3.712ZM19.513 8.199l-3.712-3.712-8.4 8.4a5.25 5.25 0 0 0-1.32 2.214l-.8 2.685a.75.75 0 0 0 .933.933l2.685-.8a5.25 5.25 0 0 0 2.214-1.32l8.4-8.4Z" />
+                                                <path d="M5.25 5.25a3 3 0 0 0-3 3v10.5a3 3 0 0 0 3 3h10.5a3 3 0 0 0 3-3V13.5a.75.75 0 0 0-1.5 0v5.25a1.5 1.5 0 0 1-1.5 1.5H5.25a1.5 1.5 0 0 1-1.5-1.5V8.25a1.5 1.5 0 0 1 1.5-1.5h5.25a.75.75 0 0 0 0-1.5H5.25Z" />
+                                                </svg>
+                                        </button>
+
+                                        <button><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
+                                                <path fillRule="evenodd" d="M16.5 4.478v.227a48.816 48.816 0 0 1 3.878.512.75.75 0 1 1-.256 1.478l-.209-.035-1.005 13.07a3 3 0 0 1-2.991 2.77H8.084a3 3 0 0 1-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 0 1-.256-1.478A48.567 48.567 0 0 1 7.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 0 1 3.369 0c1.603.051 2.815 1.387 2.815 2.951Zm-6.136-1.452a51.196 51.196 0 0 1 3.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 0 0-6 0v-.113c0-.794.609-1.428 1.364-1.452Zm-.355 5.945a.75.75 0 1 0-1.5.058l.347 9a.75.75 0 1 0 1.499-.058l-.346-9Zm5.48.058a.75.75 0 1 0-1.498-.058l-.347 9a.75.75 0 0 0 1.5.058l.345-9Z" clipRule="evenodd" />
+                                                </svg>
+                                        </button>
+                                    </td>
+                                </tr>
+                            )
+                        })}
+                    </tbody>
+                    
+
+                </table>
+            </div>}
+    </div>
+  )
+}
+
+export default PasswordRendering
